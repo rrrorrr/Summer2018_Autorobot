52d2e86e4480896b284a4448429f9d8983b8601d
LAPTOP-EI66Q0UE\の～とぱそこん <Xerrorer@gmail.com>
2018-08-25 18:08:07 +0900

    no message

--- State.h
+++ State.h
@@ -7,8 +7,30 @@ class BaseState :public Task
 {
 protected:
 	StateChanger * STATECHANGER;
+	int count1, count2;
+	bool flag1, flag2;
 public:
-	BaseState(StateChanger*changer) { STATECHANGER = changer; }
+	BaseState(StateChanger*changer) { 
+		STATECHANGER = changer;
+		ros::init(argc, argv, "checker");
+
+		std_msgs::Int16 motorUpRight;
+		std_msgs::Int16 motorDownRight;
+		std_msgs::Int16 motorUpLeft;
+		std_msgs::Int16 motorDownLeft;
+		std_msgs::Bool Air;
+
+		ros::Publisher order1 = nh.advertise<std_msgs::Int16>("motor_order1", 1);
+		ros::Publisher order2 = nh.advertise<std_msgs::Int16>("motor_order2", 1);
+		ros::Publisher order3 = nh.advertise<std_msgs::Int16>("motor_order3", 1);
+		ros::Publisher order4 = nh.advertise<std_msgs::Int16>("motor_order4", 1);
+		ros::Publisher orderAir = nh.advertise<stg_msgs::Bool>("Air_order", 1);
+
+		ros::Subscriber sub1 = nh.subscribe("fot1", 50, fotresponce1);
+		ros::Subscriber sub2 = nh.subscribe("fot2", 50, fotresponce2);
+		ros::Subscriber sub3 = nh.subscribe("touch1", 50, touchresponce1);
+		ros::Subscriber sub4 = nh.subscribe("touch2", 50, touchresponce2);
+	}
 	virtual ~BaseState(){}
 	virtual void Initialize()override{}
 	virtual void Finalize()override{}
--- States.h
+++ States.h
@@ -2,6 +2,8 @@
 #define states
 /*
 #include<ros/ros.h>
+#include<std_msgs/Int16>
+#include<std_msgs/Bool>
 #include<iostream>
 */
 #include"State.h"
@@ -9,46 +11,94 @@
 
 class State1 :public BaseState
 {
-public:
-	State1(StateChanger*changer):BaseState(changer) {}
-	void Initialize()override {
-
-	}
-	void Finalize()override {
-
-	}
-	void all()override {
-
+	class State1 :public BaseState
+	{
+	public:
+		State1(StateChanger*changer) :BaseState(changer) {}
+		void Initialize()override {
+			count1 = 0;
+			Air.data = true;
+			orderAir.publish(Air);
+			motorUpRight.data = 255;
+			motorDownRight.data = 255;
+			motorUpLeft.data = 255;
+			motorDownLeft.data = 255;
+		}
+		void Finalize()override {
+		}
+		void all()override {
+			order1.publish(motorUpRight);
+			order2.publish(motorDownRight);
+			order3.publish(motorUpLeft);
+			order4.publish(motorDownLeft);
+			nh.spinOnce();
+			if (count1 == 3) {
+				motorUpRight.data = 0;
+				motorDownRight.data = 0;
+				motorUpLeft.data = 0;
+				motorDownLeft.data = 0;
+				order1.publish(motorUpRight);
+				order2.publish(motorDownRight);
+				order3.publish(motorUpLeft);
+				order4.publish(motorDownLeft);
+				STATECHANGER->ChangeState(2);
+			}
 	}
 };
 
 class State2 :public BaseState
 {
+private:
+	bool comflag = true;
+	int checker = 0;
 public:
-	State2(StateChanger*changer) :BaseState(changer){}
+	State2(StateChanger*changer) :BaseState(changer) {}
 	void Initialize()override {
-
+		Air.data = false;
 	}
 	void Finalize()override {
 
 	}
 	void all()override {
-
+		if (comflag == true)orderAir.publish(Air); comflag = false;
+		nh.spinOnce();
+		checker++;
+		if (checker == 300)STATECHANGER->ChangeState(3);
 	}
 };
 
 class State3 :public BaseState
 {
 public:
-	State3(StateChanger*changer) :BaseState(changer){}
+	State3(StateChanger*changer) :BaseState(changer) {}
 	void Initialize()override {
-
+		motorUpRight.data = 255;
+		motorDownRight.data = 255;
+		motorUpLeft.data = 255;
+		motorDownLeft.data = 255;
+		flag1 = false;
 	}
 	void Finalize()override {
-
+		Air.data = true;
+		orderAir.publish(Air);
 	}
 	void all()override {
-
+		order1.publish(motorUpRight);
+		order2.publish(motorDownRight);
+		order3.publish(motorUpLeft);
+		order4.publish(motorDownLeft);
+		nh.spinOnce();
+		if (flag1) {
+			motorUpRight.data = 0;
+			motorDownRight.data = 0;
+			motorUpLeft.data = 0;
+			motorDownLeft.data = 0;
+			order1.publish(motorUpRight);
+			order2.publish(motorDownRight);
+			order3.publish(motorUpLeft);
+			order4.publish(motorDownLeft);
+			STATECHANGER->ChangeState(4);
+		}
 	}
 };
 
@@ -57,42 +107,82 @@ class State4 :public BaseState
 public:
 	State4(StateChanger*changer) :BaseState(changer){}
 	void Initialize()override {
-
+		motorUpRight.data = 255;
+		motorDownRight.data = 255;
+		motorUpLeft.data = 255;
+		motorDownLeft.data = 255;
+		count2 = 0;
 	}
 	void Finalize()override {
-
 	}
 	void all()override {
-
+		order1.publish(motorUpRight);
+		order2.publish(motorDownRight);
+		order3.publish(motorUpLeft);
+		order4.publish(motorDownLeft);
+		nh.spinOnce();
+		if (count2 == 2) {
+			motorUpRight.data = 0;
+			motorDownRight.data = 0;
+			motorUpLeft.data = 0;
+			motorDownLeft.data = 0;
+			order1.publish(motorUpRight);
+			order2.publish(motorDownRight);
+			order3.publish(motorUpLeft);
+			order4.publish(motorDownLeft);
+			STATECHANGER->ChangeState(5);
+		}
 	}
 };
 
 class State5 :public BaseState
 {
 public:
-	State5(StateChanger*changer) :BaseState(changer){}
+	State5(StateChanger*changer) :BaseState(changer) {}
 	void Initialize()override {
-
+		flag2 = false;
 	}
 	void Finalize()override {
-
+		Air.data = false;
+		orderAir.publish(Air);
 	}
 	void all()override {
-
+		nh.spinOnce();
+		if (flag2) STATECHANGER->ChangeState(6);
 	}
 };
 
 class State6 :public BaseState
 {
 public:
-	State6(StateChanger*changer) :BaseState(changer){}
+	State6(StateChanger*changer) :BaseState(changer) {}
 	void Initialize()override {
-
+		count2 = 0;
+		motorUpRight.data = 255;
+		motorDownRight.data = 255;
+		motorUpLeft.data = 255;
+		motorDownLeft.data = 255;
 	}
 	void Finalize()override {
 
 	}
 	void all()override {
+		order1.publish(motorUpRight);
+		order2.publish(motorDownRight);
+		order3.publish(motorUpLeft);
+		order4.publish(motorDownLeft);
+		nh.spinOnce();
+		if (count2 == 2) {
+			motorUpRight.data = 0;
+			motorDownRight.data = 0;
+			motorUpLeft.data = 0;
+			motorDownLeft.data = 0;
+			order1.publish(motorUpRight);
+			order2.publish(motorDownRight);
+			order3.publish(motorUpLeft);
+			order4.publish(motorDownLeft);
+			STATECHANGER->ChangeState(7);
+		}
 
 	}
 };
@@ -100,19 +190,35 @@ public:
 class State7 :public BaseState
 {
 public:
-	State7(StateChanger*changer) :BaseState(changer){}
-	void Initialize()override;
-	void Finalize()override;
-	void all()override;
-};
+	State7(StateChanger*changer) :BaseState(changer) {}
+	void Initialize()override {
+		count1 = 0;
+		motorUpRight.data = 255;
+		motorDownRight.data = 255;
+		motorUpLeft.data = 255;
+		motorDownLeft.data = 255;
+	}
+	void Finalize()override {
 
-class EndState :public BaseState
-{
-public:
-	EndState(StateChanger*changer) :BaseState(changer){}
-	void Initialize()override;
-	void Finalize()override;
-	void all()override;
+	}
+	void all()override {
+		order1.publish(motorUpRight);
+		order2.publish(motorDownRight);
+		order3.publish(motorUpLeft);
+		order4.publish(motorDownLeft);
+		nh.spinOnce();
+		if (count1 == 2) {
+			motorUpRight.data = 0;
+			motorDownRight.data = 0;
+			motorUpLeft.data = 0;
+			motorDownLeft.data = 0;
+			order1.publish(motorUpRight);
+			order2.publish(motorDownRight);
+			order3.publish(motorUpLeft);
+			order4.publish(motorDownLeft);
+			STATECHANGER->ChangeState(10);
+		}
+	}
 };
 #endif
 
